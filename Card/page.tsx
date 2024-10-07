b42638908ea339e5dce87980720418e1f29faa79
8noor <nanum3773@gmail.com>
2024-10-07 08:35:30 +0500

    id card 2 next js

--- Card/page.tsx
+++ Card/page.tsx
@@ -1,28 +1,26 @@
-import React from 'react'
+import React from "react";
 
-interface props{
-    name: string;
-    rollno: number;
-    age: number;
-    day: string;
-    class: number;
+interface props {
+  name: string;
+  rollNo: number;
+  age: number;
+  day: string;
+  class: number;
 }
 
-
-
-
-const page = (props:props) => {
+export default function Card(props: props) {
   return (
     <div>
-        <h2><b>Student Id Card</b></h2>
-        <br /> 
+      <h2>
+        <b>Student Id Card</b>
+      </h2>
+      <br />
       <h1>Name:{props.name}</h1>
-      <h1>RollNo:{props.rollno}</h1>
+      <h1>RollNo:{props.rollNo}</h1>
       <h1>Age: {props.age}</h1>
       <h1>Day:{props.day}</h1>
       <h1>Class:{props.class}</h1>
     </div>
-  )
+  );
 }
 
-export default page
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import Image from "next/image";
+import React from "react";
+
 import Card from "../../Card/page";
 
 export default function Home() {
@@ -7,13 +8,17 @@ export default function Home() {
       <div className="flex-grow flex items-center justify-center p-6 ">
         <div className=" bg-slate-200 p-12 rounded-lg shadow-lg max-w-4x1 w-full flex items-center justify-center border-4 border-sky-300">
           <div className=" w-52 h-42 ml-8 mt-8 pl-3 pt-4 text-blue-800 bg-sky-200 ">
-            <Card name="Anum" rollno={123456} day="Monday" age={20} class={4} />
+            <Card name="Anum"
+             rollNo={123456}
+              day="Monday" 
+              age={20} 
+              class={4} />
           </div>
           <br />
           <div className=" w-52 h-42 ml-8 mt-8 pl-3 pt-4 text-blue-800 bg-sky-200  ">
             <Card
               name="Noor"
-              rollno={122222}
+              rollNo={122222}
               day="Tuesday"
               age={21}
               class={4}
@@ -22,8 +27,8 @@ export default function Home() {
           <br />
           <div className=" w-52 h-42 ml-8 mt-8 pl-3 pt-4 text-blue-800 bg-sky-200 ">
             <Card
-              name="Ather"
-              rollno={66666}
+              name="Nazia"
+              rollNo={66666}
               day="Wednesday"
               age={22}
               class={4}
@@ -32,7 +37,7 @@ export default function Home() {
           <div className=" w-52 h-42 ml-8 mt-8 pl-3 pt-4 text-blue-800 bg-sky-200 ">
             <Card
               name="Saeed"
-              rollno={454545}
+              rollNo={454545}
               day="Thursday"
               age={23}
               class={4}
@@ -41,7 +46,7 @@ export default function Home() {
           <div className=" w-52 h-42 ml-8 mt-8 pl-3 pt-4 text-blue-800 bg-sky-200 ">
             <Card
               name="Ahmed"
-              rollno={558855}
+              rollNo={558855}
               day="Friday"
               age={24}
               class={4}
